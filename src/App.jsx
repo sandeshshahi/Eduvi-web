15915e5d5bde82c10e4c16445bca5f23ba80b4d3
Sandesh Shahi <sndshshahi@gmail.com>
2024-04-25 15:56:25 +0545

    Complete videocall section and add arrow button,call button,baby image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import VideoCallSection from "./components/classes/videoCall/VideoCallSection";
 import "./css/style.min.css";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <>
-      <LandingPage />
+      {/* <LandingPage /> */}
+      <VideoCallSection />
     </>
   );
 }
---
+++ src/components/classes/videoCall/VideoCallSection.jsx
@@ -0,0 +1,56 @@
+import boardImage from "../../../../public/images/boardImage.png";
+import babyImage from "../../../../public/images/babyImage.png";
+import pattern from "../../../../public/images/pattern.png";
+import callButton from "../../../assets/icons/callButton.svg";
+import arrowButton from "../../../assets/icons/circularArrowButton.svg";
+
+const VideoCallSection = () => {
+  return (
+    <div className="videoCallSection">
+      {/* board-image and baby-image  */}
+      <div className="videoCallSection-boardImage">
+        <img
+          className="videoCallSection-boardImage-image"
+          src={boardImage}
+          alt=" board image"
+          height={570}
+          width={1000}
+        />
+
+        <div className="videoCallSection-boardImage-buttons">
+          <div className="videoCallSection-babyImage">
+            <img
+              className="videoCallSection-babyImage-image"
+              src={babyImage}
+              alt="baby image"
+              height={194.4}
+              width={158.4}
+            />
+          </div>
+          <div className="videoCallSection-callButton">
+            <img
+              src={callButton}
+              alt="circular call button"
+              height={60}
+              width={60}
+            />
+          </div>
+          <div className="videoCallSection-arrowButton">
+            <img
+              src={arrowButton}
+              alt="circular arrow button"
+              height={60}
+              width={60}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* for pattern */}
+      <div className="videoCallSection__pattern">
+        <img src={pattern} alt="pattern" height={185} width={180} />
+      </div>
+    </div>
+  );
+};
+export default VideoCallSection;
